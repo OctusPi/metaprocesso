3a1c2f62160dc2c52cb91f561a712d282362bc88
OctusPi <octuspi@gmail.com>
2024-03-05 01:18:40 -0300

    feat: main top header base layout

--- client/src/components/MainHeader.vue
+++ client/src/components/MainHeader.vue
@@ -2,39 +2,60 @@
 </script>
 
 <template>
-    <header class="header-main d-md-flex flex-reverse justify-content-between align-items-center">
-        <div class="header-main-action md-my-2">
-            <button type="button">1</button>
-            <button type="button">2</button>
+    <header class="header-main d-md-flex justify-content-between align-items-center">
+        <div class="header-main-title d-flex align-items-center flex-fill">
+            <i class="bi nav-link-icon bi-grid me-2 rounded-circle txt-color-base"></i>
+            <div>
+                <h1 class="p-0 m-0 txt-color">Title Page</h1>
+                <span class="m-0 p-0 small txt-color-sec">Description about functionality page</span>
+            </div>
+        </div>
 
-            <div class="btn-group" role="group">
-                <button type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <i class="bi bi-person-circle"></i>
+        <div class="header-main-searchbar flex-fill">
+            <div class="dropdown w-100">
+                <button class="header-main-btn-searchbar rounded-4" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="bi bi-search"></i> Localizar
                 </button>
+
                 <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Dropdown link</a></li>
-                    <li><a class="dropdown-item" href="#">Dropdown link</a></li>
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
                 </ul>
             </div>
         </div>
-        <div class="header-main-title d-flex align-items-center">
-            <i class="bi nav-link-icon bi-grid me-2"></i>
-            <div>
-                <h1 class="p-0 m-0">Title Page</h1>
-                <span class="m-0 p-0 small">Description about functionality page</span>
-            </div>
-        </div>
 
-        <div class="header-main-searchbar px-4">
-            <form class="row g-3">
-                <div class="input-group">
-                    <div class="input-group-text" id="btnGroupAddon">
-                        <i class="bi bi-search"></i>
-                    </div>
-                    <input type="text" class="form-control" placeholder="Localizar"
-                        aria-label="Input group example" aria-describedby="btnGroupAddon">
-                </div>
-            </form>
+        <div class="header-main-action my-md-2 d-flex flex-fill justify-content-end">
+            <div class="dropdown">
+  <button class="btn btn-secondary" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    <i class="bi bi-bell"></i>
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+</div>
+<div class="dropdown">
+  <button class="btn btn-secondary" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    <i class="bi bi-moon-stars"></i>
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+</div>
+<div class="dropdown">
+  <button class="btn btn-secondary" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    <i class="bi bi-person-circle"></i>
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+</div>
         </div>
     </header>
 </template>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -11,7 +11,7 @@ import MainHeader from '@/components/MainHeader.vue';
         <p>ssd</p>
         <p>ssd</p>
         <p>ssd</p>
-        <p>ssd</p>
+        <p>ssduuuuuuuuuuu u isufsi idusf skj kvx iudfhsiduf sohsdjfskf iuhfs fius bvx icu sdfsd fos uisdfsi dsdn i vsdiubfs dfsoidf iugsif sov bisudf dsihfsiod vosdifh svnoidsnfoidshf isd ondviosd jfsdf nfsa fiasd fiasudfn sidfn isduf dsifooifus fo asdiouf bodasubf oidasu fasudio bfsaduiobf asduio bfsduofbo isdufb sdioufb osduifbasdufbi uosd bfuaebyufo boiasudb fuioabsdfo udbsi fuasdo bfuiasd fiousbfioayebfoibsdafbiausbdiu obafuabsdfudi sifbasduiofuiod bsfioudabs fausdib </p>
         <p>ssd</p>
         <p>ssd</p>
         <p>ssd</p>
