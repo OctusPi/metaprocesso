b0430ebbd63c91c0ec44bbc52b5700d60c605e7c
renan <alvesrenanpro@gmail.com>
2024-03-09 02:08:29 -0300

    feat: UserModelTest with fake DB

--- server/app/Providers/AppServiceProvider.php
+++ server/app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Models\Organ;
-use App\Models\Unit;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
---
+++ server/tests/Unit/UserModelTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Illuminate\Database\QueryException;
+use Tests\TestCase;
+
+class UserModelTest extends TestCase
+{
+    public function test_user_fullfilled(): void
+    {
+        $user = (new User())->fill([
+            'name' => fake()->name(),
+            'email' => fake()->email(),
+            'password' => fake()->password(),
+            'username' => fake()->userName(),
+            'sectors' => ['sector'],
+            'organs' => ['organ'],
+            'units' => ['unit'],
+            'modules' => ['module'],
+            'profile' => 1,
+            'status' => 1
+        ]);
+
+        $this->assertTrue($user->save());   
+    }
+
+    public function test_user_nullables(): void
+    {
+        $user = (new User())->fill([
+            'name' => fake()->name(),
+            'email' => fake()->email(),
+            'password' => fake()->password(),
+            'username' => fake()->userName(),
+            'sectors' => ['sector'],
+            'profile' => 1,
+            'status' => 1
+        ]);
+
+        $this->assertTrue($user->save());  
+    }
+
+    public function test_user_invalid(): void
+    {
+        $user = (new User())->fill([
+            'name' => fake()->name(),
+            'email' => fake()->email(),
+            'password' => fake()->password(),
+            'username' => fake()->userName(),
+            'sectors' => null,
+            'profile' => 1,
+            'status' => 1
+        ]);
+
+        $this->assertThrows($user->save(...), QueryException::class);
+    }
+}
