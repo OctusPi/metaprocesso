ab45ae43cb13d901a4ff1bd9864e5a7b719c6756
renan <alvesrenanpro@gmail.com>
2024-03-12 12:10:40 -0300

    feat(Utils/Dates): add convert functionality

--- server/app/Utils/Dates.php
+++ server/app/Utils/Dates.php
@@ -3,11 +3,48 @@
 namespace App\Utils;
 
 use Carbon\Carbon;
+use DateException;
 
 class Dates
 {
+    const UTC = "Y-m-d";
+    const UTC_TIME = "Y-m-d H:i:s";
+    const PTBR = "d/m/Y";
+    const PTBR_TIME = "d/m/Y H:i:s";
+
+    public Carbon $date;
+
+    /**
+     * Creates a Carbon date instance
+     * @param string $format Defines the date format
+     * @param string $date Defines the date string with the given **$format** param
+     */
+
+    public function __construct(string $format, string $date)
+    {
+        if (!Carbon::canBeCreatedFromFormat($date, $format)) {
+            throw new DateException("The given date does not match the format");
+        }
+
+        $this->date = Carbon::createFromFormat($format, $date);
+    }
+
+    /**
+     * Returns the current UTC datetime value
+     * @return string
+     */
     public static function nowUTC(): string
     {
         return Carbon::now('UTC')->toDateTimeString();
+    }
+
+    /**
+     * Converts a Pt-BR date string to UTC format
+     * @param string $to Defines the expected format
+     * @return string
+     */
+    public function convertTo(string $to): string
+    {
+        return $this->date->format($to);
     }
 }
---
+++ server/tests/Unit/DatesUtilTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Utils\Dates;
+use Carbon\Carbon;
+use DateException;
+use DateTime;
+use PHPUnit\Framework\TestCase;
+
+class DatesUtilTest extends TestCase
+{
+    public function test_nowutc(): void
+    {
+        $this->assertTrue(
+            Carbon::createFromFormat(Dates::UTC_TIME, Dates::nowUTC())->isValid()
+        );
+    }
+
+    public function test_convert(): void
+    {
+        $dateUtc = new Dates(Dates::UTC, "2023-12-12");
+        $dateTimeUtc = new Dates(Dates::UTC_TIME, "2023-12-12 12:12:12");
+        $datePtbr = new Dates(Dates::PTBR, "12/12/2023");
+        $dateTimePtbr = new Dates(Dates::PTBR_TIME, "12/12/2023 12:12:12");
+
+
+        $this->assertEquals($dateUtc->convertTo(Dates::PTBR), "12/12/2023");
+        $this->assertEquals($dateTimeUtc->convertTo(Dates::PTBR), "12/12/2023");
+        $this->assertEquals($datePtbr->convertTo(Dates::UTC), "2023-12-12");
+        $this->assertEquals($dateTimePtbr->convertTo(Dates::UTC), "2023-12-12");
+    }
+
+    public function test_invalid_convert_utc(): void
+    {
+        $this->expectException(DateException::class);
+
+        $invalidDateUtc = new Dates(Dates::UTC, "10/20");
+        $invalidDateUtc->convertTo(Dates::PTBR);
+    }
+
+    public function test_invalid_convert_ptbr(): void
+    {
+        $this->expectException(DateException::class);
+
+        $invalidDatePtbr = new Dates(Dates::PTBR, "12/12");
+        $invalidDatePtbr->convertTo(Dates::UTC);
+    }
+}
