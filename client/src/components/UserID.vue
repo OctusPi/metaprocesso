c95bb67ec218c53ec24c5dd6e3df40cfed6d2edd
OctusPi <octuspi@gmail.com>
2024-03-22 12:11:03 -0300

    chore: Component userid, timesession count and logout system

--- client/src/components/UserID.vue
+++ client/src/components/UserID.vue
@@ -1,4 +1,38 @@
 <script setup>
+    import { useJwt } from '@/stores/auth';
+import { onMounted, ref } from 'vue';
+
+    const auth = useJwt()
+    const user = auth.getUser() ?? {}
+    const timesession = ref('60:00')
+
+    function logout(){
+        auth.clear()
+        window.location = '/'
+    }
+
+    onMounted(() => {
+        
+        let minutes = 59
+        let seconds = 59
+        
+        function countsession(){
+            if(seconds > 0){
+                seconds--
+            }else{
+                seconds = 59
+                minutes--
+            }
+
+            timesession.value = minutes >= 0 ? minutes.toString().padStart(2, '0')+':'+seconds.toString().padStart(2, '0') : '00:00'
+
+            if(minutes < 0){
+                logout()
+            }
+        }
+
+        setInterval(countsession, 1000);
+    })
 
 </script>
 
@@ -8,9 +42,51 @@
             <i class="bi bi-person-circle"></i>
         </button>
         <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li>
+                <a class="dropdown-item d-flex align-items-center" href="#">
+                    <i class="bi bi-person-workspace me-3 fs-5"></i>
+                    <span>
+                        <span class="d-block p-0 m-0 small">Usuário</span>
+                        <span class="d-block p-0 m-0 small fw-bolder">{{ user.name }}</span>
+                    </span>
+                </a>
+            </li>
+            <li>
+                <a class="dropdown-item d-flex align-items-center" href="#">
+                    <i class="bi bi-gear me-3 fs-5"></i>
+                    <span>
+                        <span class="d-block p-0 m-0 small">Perfil</span>
+                        <span class="d-block p-0 m-0 small fw-bolder">{{ user.profile }}</span>
+                    </span>
+                </a>
+            </li>
+            <li>
+                <a class="dropdown-item d-flex align-items-center" href="#">
+                    <i class="bi bi-calendar-week me-3 fs-5"></i>
+                    <span>
+                        <span class="d-block p-0 m-0 small">Ultimo Acesso</span>
+                        <span class="d-block p-0 m-0 small fw-bolder">{{ user.last_login }}</span>
+                    </span>
+                </a>
+            </li>
+            <li>
+                <a class="dropdown-item d-flex align-items-center" href="#">
+                    <i class="bi bi-alarm me-3 fs-5"></i>
+                    <span>
+                        <span class="d-block p-0 m-0 small">Tempo Restante</span>
+                        <span class="d-block p-0 m-0 small fw-bolder">{{ timesession }}</span>
+                    </span>
+                </a>
+            </li>
+            <li>
+                <a @click="logout" class="dropdown-item d-flex align-items-center" href="#">
+                    <i class="bi bi-door-open me-3 fs-5"></i>
+                    <span>
+                        <span class="d-block p-0 m-0 small">Fazer Logout</span>
+                        <span class="d-block p-0 m-0 small fw-bolder">Sair com Segurança</span>
+                    </span>
+                </a>
+            </li>
         </ul>
     </div>
 </template>
