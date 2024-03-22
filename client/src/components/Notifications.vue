3204ca27eba4f05de07f203538036f9247182756
OctusPi <octuspi@gmail.com>
2024-03-22 19:18:21 -0300

    feat: notification component

--- client/src/components/Notifications.vue
+++ client/src/components/Notifications.vue
@@ -7,9 +7,10 @@
             <i class="bi bi-bell-fill"></i>
         </button>
         <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">
+                <i class="bi bi-app-indicator me-3"></i>
+                <span class="fs-6">Nenhuma Notificação no Momento</span>
+            </a></li>
         </ul>
     </div>
 </template>
