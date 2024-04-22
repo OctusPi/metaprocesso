2563ba2014d105804793a6c68a793ad1357f8909
octuspi <octuspi@gmail.com>
2024-04-22 07:55:58 -0300

    feat: add migrations to ordinator, demandant and comission

---
+++ server/app/Models/ComissionEnd.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ComissionEnd extends Model
+{
+    use HasFactory;
+
+    protected $table = 'comissions_ends';
+}
---
+++ server/database/migrations/2024_04_22_103844_comissions_members.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comissions_members', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('organ_id')->constrained('organs');
+            $table->foreignId('unit_id')->constrained('units');
+            $table->foreignId('comission_id')->constrained('comissions');
+            $table->string('name');
+            $table->integer('responsibility');
+            $table->string('document')->nullable();
+            $table->string('description')->nullable();
+            $table->date('start_term');
+            $table->date('end_term')->nullable();
+            $table->boolean('status')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comissions_members');
+    }
+};
