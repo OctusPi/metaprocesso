57786444cf8d148255f8e29a1c4af15890ca21b2
octuspi <octuspi@gmail.com>
2024-04-24 10:15:38 -0300

    chore add controller/view to Demandants

--- client/src/views/DemandantsView.vue
+++ client/src/views/DemandantsView.vue
@@ -3,6 +3,7 @@ import { onMounted, ref, watch } from 'vue'
 import forms from '@/services/forms';
 import notifys from '@/utils/notifys';
 import http from '@/services/http';
+import masks from '@/utils/masks';
 
 import MainNav from '@/components/MainNav.vue';
 import MainHeader from '@/components/MainHeader.vue';
@@ -11,31 +12,36 @@ import ManagementNav from '@/components/ManagementNav.vue';
 
 const emit = defineEmits(['callAlert', 'callRemove'])
 const props = defineProps({
-    datalist: {type: Array, default: () => []}
+    datalist: { type: Array, default: () => [] }
 })
 
 const page = ref({
-    title       :{primary: '', secondary: ''},
-    uiview      :{register:false, search:false},
-    data        :{},
-    datalist    :props.datalist,
-    dataheader  :[
-        {key:'name', title:'IDENTIFICAÇÃO'}, 
-        {key:'unit_id', title:'VINCULO', sub:[{key:'organ_id'}]},
-        {key:'description', title:'DESCRIÇÃO'}, 
+    title: { primary: '', secondary: '' },
+    uiview: { register: false, search: false },
+    data: {},
+    datalist: props.datalist,
+    dataheader: [
+        { key: 'name', title: 'IDENTIFICAÇÃO', sub: [{ key: 'cpf' }]},
+        { key: 'unit_id', title: 'VINCULO', sub: [{ key: 'organ_id' }] },
+        { key: 'status', title: 'STATUS' , sub: [{ key: 'start_term' }, { key: 'end_term' }] },
     ],
-    search      :{},
-    selects     :{
-        organs: [],
-        units: []
+    search: {},
+    selects: {
+        organs:  [],
+        units:   [],
+        sectors: [],
+        status:  []
     },
-    rules       :{
+    rules: {
         fields: {
-            name:'required',
-            organ_id:'required',
-            unit_id:'required'
+            name: 'required',
+            cpf: 'required',
+            organ_id: 'required',
+            unit_id: 'required',
+            status: 'required',
+            'start_term': 'required'
         },
-        valids:{}
+        valids: {}
     }
 })
 
@@ -43,45 +49,45 @@ watch(() => props.datalist, (newdata) => {
     page.value.datalist = newdata
 })
 
-function toggleUI(mode = null){
-    
+function toggleUI(mode = null) {
+
     switch (mode) {
         case 'register':
-            page.value.uiview.search   = false
+            page.value.uiview.search = false
             page.value.uiview.register = !page.value.uiview.register
             page.value.data = {}
             break;
         case 'update':
-            page.value.uiview.search   = false
+            page.value.uiview.search = false
             page.value.uiview.register = !page.value.uiview.register
             break;
         case 'search':
-            page.value.uiview.search   = !page.value.uiview.search
+            page.value.uiview.search = !page.value.uiview.search
             page.value.uiview.register = false
             break;
         default:
             page.value.uiview.register = false
             break;
     }
-    
-    if(page.value.uiview.register){
-        page.value.title.primary = 'Registro de Setores'
-        page.value.title.secondary = 'Insira os dados para registro de Setores vinculados as Unidades do Orgão'
-    }else{
-        page.value.title.primary = 'Listagem de Setores'
-        page.value.title.secondary = `Foram localizados ${(page.value.datalist.length).toString().padStart(2, '0')} setores inseridos no sistema`
+
+    if (page.value.uiview.register) {
+        page.value.title.primary = 'Registro de Demandantes'
+        page.value.title.secondary = 'Insira os dados de Demandantes vinculados as Unidades do Orgão'
+    } else {
+        page.value.title.primary = 'Listagem de Demandantes'
+        page.value.title.secondary = `Foram localizados ${(page.value.datalist.length).toString().padStart(2, '0')} Demandantes inseridos no sistema`
     }
 }
 
-function save(){
+function save() {
     const validation = forms.checkform(page.value.data, page.value.rules);
-    if(!validation.isvalid){
+    if (!validation.isvalid) {
         emit('callAlert', notifys.warning(validation.message))
         return
     }
 
-    const data = {...page.value.data }
-    const url  = page.value.data?.id ? '/sectors/update' : '/sectors/save'
+    const data = { ...page.value.data }
+    const url  = page.value.data?.id ? '/demandants/update' : '/demandants/save'
     const exec = page.value.data?.id ? http.put : http.post
 
     exec(url, data, emit, () => {
@@ -89,35 +95,59 @@ function save(){
     })
 }
 
-function update(id){
-    http.get(`/sectors/details/${id}`, emit, (response) => {
+function update(id) {
+    http.get(`/demandants/details/${id}`, emit, (response) => {
         selects('organ_id', response.data?.unit)
         page.value.data = response.data
         toggleUI('update')
     })
 }
 
-function remove(id){
+function remove(id) {
     emit('callRemove', {
-        id      : id,
-        url     : '/sectors',
-        search  : page.value.search
+        id: id,
+        url: '/demandants',
+        search: page.value.search
     })
 }
 
-function list(){
-    http.post('/sectors/list', page.value.search, emit, (response) => {
+function list() {
+    http.post('/demandants/list', page.value.search, emit, (response) => {
         page.value.datalist = response.data ?? []
         toggleUI('list')
     })
 }
 
-function selects(key = null, search = null){
+function selects(key = null, search = null) {
 
-    const urlselect = (key && search) ? `/sectors/selects/${key}/${search}` : '/sectors/selects'
+    const urlselect = (key && search) ? `/demandants/selects/${key}/${search}` : '/demandants/selects'
 
     http.get(urlselect, emit, (response) => {
         page.value.selects = response.data
+    })
+}
+
+function handleFile(event){
+    const file = event.target.files[0]
+    if(file){
+        page.value.data.document = file
+    }
+}
+
+function download(id){
+    http.download(`/demandants/download/${id}`, emit, (response) => {
+        if(response.headers['content-type'] !== 'application/pdf'){
+            emit('callAlert', notifys.warning('Arquivo Indisponível'))
+            return
+        }
+        
+        const url  = URL.createObjectURL(new Blob([response.data], {type: 'application/pdf'}))
+        const link = document.createElement('a')
+        link.href = url;
+        link.download = `Amparo-${id}.pdf`
+        document.body.appendChild(link)
+        link.click()
+        window.URL.revokeObjectURL(url);
     })
 }
 
@@ -133,12 +163,13 @@ onMounted(() => {
         <MainNav />
         <section class="container-main">
             <MainHeader :header="{
-                icon: 'bi-grid',
-                title: 'Gerenciamento de Setor',
-                description: 'Registro de setores vinculados as Unidades do Orgão'
+                icon: 'bi-person-video2',
+                title: 'Gerenciamento de Demandantes',
+                description: 'Registro de Demandantes vinculados as Unidades do Orgão'
             }" />
 
             <div class="box p-0 mb-4 rounded-4">
+                
                 <!--HEDER PAGE-->
                 <div class="d-md-flex justify-content-between align-items-center px-4 px-md-5 pt-5 mb-4">
                     <div class="info-list">
@@ -147,11 +178,13 @@ onMounted(() => {
                     </div>
                     <div class="action-buttons d-flex my-2">
                         <ManagementNav />
-                        <button @click="toggleUI('register')" type="button" class="btn btn-action btn-action-primary ms-2">
+                        <button @click="toggleUI('register')" type="button"
+                            class="btn btn-action btn-action-primary ms-2">
                             <i class="bi bi-plus-circle"></i>
                             <span class="title-btn-action ms-2 d-none d-md-block d-lg-inline">Adicionar</span>
                         </button>
-                        <button @click="toggleUI('search')" type="button" class="btn btn-action btn-action-primary ms-2">
+                        <button @click="toggleUI('search')" type="button"
+                            class="btn btn-action btn-action-primary ms-2">
                             <i class="bi bi-search"></i>
                             <span class="title-btn-action ms-2 d-none d-md-block d-lg-inline">Pesquisar</span>
                         </button>
@@ -168,20 +201,19 @@ onMounted(() => {
                         </div>
                         <div class="col-sm-12 col-md-4">
                             <label for="s-organ_id" class="form-label">Orgão</label>
-                            <select name="organ_id" class="form-control" id="s-organ_id"
-                            v-model="page.search.organ_id"
-                            @change="selects('organ_id', page.search.organ_id)">
-                            <option value=""></option>
-                            <option v-for="o in page.selects.organs" :key="o.id" :value="o.id">{{ o.title }}</option>
-                        </select>
+                            <select name="organ_id" class="form-control" id="s-organ_id" v-model="page.search.organ_id"
+                                @change="selects('organ_id', page.search.organ_id)">
+                                <option value=""></option>
+                                <option v-for="o in page.selects.organs" :key="o.id" :value="o.id">{{ o.title }}
+                                </option>
+                            </select>
                         </div>
                         <div class="col-sm-12 col-md-4">
                             <label for="s-unit_id" class="form-label">Unidade</label>
-                            <select name="unit_id" class="form-control" id="s-unit_id"
-                            v-model="page.search.unit_id">
-                            <option value=""></option>
-                            <option v-for="o in page.selects.units" :key="o.id" :value="o.id">{{ o.title }}</option>
-                        </select>
+                            <select name="unit_id" class="form-control" id="s-unit_id" v-model="page.search.unit_id">
+                                <option value=""></option>
+                                <option v-for="o in page.selects.units" :key="o.id" :value="o.id">{{ o.title }}</option>
+                            </select>
                         </div>
 
                         <div class="d-flex flex-row-reverse mt-4">
@@ -194,12 +226,12 @@ onMounted(() => {
                 <!--BOX LIST-->
                 <div v-if="!page.uiview.register" id="list-box" class="mb-4">
                     <TableList 
-                    @action:update="update" 
-                    @action:delete="remove" 
-                    :header="page.dataheader" 
-                    :body="page.datalist" 
-                    :actions="['update', 'delete']"
-                    :casts="{ 'organ_id':page.selects.organs, 'unit_id':page.selects.units} "/>
+                        @action:update="update" 
+                        @action:delete="remove"
+                        @action:download="download"
+                        :header="page.dataheader"
+                        :body="page.datalist" :actions="['download', 'update', 'delete']"
+                        :casts="{ 'organ_id': page.selects.organs, 'unit_id': page.selects.units, 'status':page.selects.status}" />
                 </div>
 
                 <!--BOX REGISTER-->
@@ -211,14 +243,28 @@ onMounted(() => {
                                 <label for="name" class="form-label">Nome</label>
                                 <input type="text" name="name" class="form-control"
                                     :class="{ 'form-control-alert': page.rules.valids.name }" id="name"
-                                    placeholder="Nome de identificação do Setor" v-model="page.data.name">
+                                    placeholder="Nome do Ordenador" v-model="page.data.name">
                             </div>
+                            <div class="col-sm-12 col-md-4">
+                                <label for="cpf" class="form-label">CPF</label>
+                                <input type="text" name="cpf" class="form-control"
+                                    :class="{ 'form-control-alert': page.rules.valids.cpf }" id="cpf"
+                                    placeholder="000.000.000-00" v-maska:[masks.maskcpf] v-model="page.data.cpf">
+                            </div>
+                            <div class="col-sm-12 col-md-4">
+                                <label for="registration" class="form-label">Matrícula</label>
+                                <input type="text" name="registration" class="form-control"
+                                    :class="{ 'form-control-alert': page.rules.valids.registration }" id="registration"
+                                    placeholder="Número da Matrícula Servidor" v-model="page.data.registration">
+                            </div>
+                        </div>
+
+                        <div class="row mb-3 g-3">
                             <div class="col-sm-12 col-md-4">
                                 <label for="organ_id" class="form-label">Orgão</label>
                                 <select name="organ_id" class="form-control"
                                     :class="{ 'form-control-alert': page.rules.valids.organ_id }" id="organ_id"
-                                    v-model="page.data.organ_id"
-                                    @change="selects('organ_id', page.data.organ_id)">
+                                    v-model="page.data.organ_id" @change="selects('organ_id', page.data.organ_id)">
                                     <option value=""></option>
                                     <option v-for="s in page.selects.organs" :value="s.id" :key="s.id">{{ s.title }}
                                     </option>
@@ -227,6 +273,7 @@ onMounted(() => {
                             <div class="col-sm-12 col-md-4">
                                 <label for="unit_id" class="form-label">Unidade</label>
                                 <select name="unit_id" class="form-control"
+                                    @change="selects('unit_id', page.data.unit_id)"
                                     :class="{ 'form-control-alert': page.rules.valids.unit_id }" id="unit_id"
                                     v-model="page.data.unit_id">
                                     <option value=""></option>
@@ -234,14 +281,45 @@ onMounted(() => {
                                     </option>
                                 </select>
                             </div>
+                            <div class="col-sm-12 col-md-4">
+                                <label for="sector_id" class="form-label">Setor</label>
+                                <select name="sector_id" class="form-control" id="sector_id"
+                                    v-model="page.data.sector_id">
+                                    <option value=""></option>
+                                    <option v-for="s in page.selects.sectors" :value="s.id" :key="s.id">{{ s.title }}
+                                    </option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="row mb-3 g-3">
+                            <div class="col-sm-12 col-md-4">
+                                <label for="start_term" class="form-label">Inicio Cargo</label>
+                                <input type="text" name="start_term" class="form-control"
+                                    :class="{ 'form-control-alert': page.rules.valids.start_term }" id="start_term"
+                                    placeholder="dd/mm/aaaa" v-maska:[masks.maskdate] v-model="page.data.start_term">
+                            </div>
+                            <div class="col-sm-12 col-md-4">
+                                <label for="end_term" class="form-label">Término Cargo</label>
+                                <input type="text" name="end_term" class="form-control" id="end_term"
+                                    placeholder="dd/mm/aaaa" v-maska:[masks.maskdate] v-model="page.data.end_term">
+                            </div>
+                            <div class="col-sm-12 col-md-4">
+                                <label for="status" class="form-label">Status</label>
+                                <select name="status" class="form-control"
+                                    :class="{ 'form-control-alert': page.rules.valids.status }" id="status"
+                                    v-model="page.data.status">
+                                    <option value=""></option>
+                                    <option v-for="s in page.selects.status" :value="s.id" :key="s.id">{{ s.title }}
+                                    </option>
+                                </select>
+                            </div>
                         </div>
 
                         <div class="row mb-3 g-3">
                             <div class="col-sm-12">
-                                <label for="description" class="form-label">Descrição</label>
-                                <input type="text" name="description" class="form-control" id="description"
-                                    placeholder="Tipo de Setor: Escola Municipal, UBS, Setor Administrativo" 
-                                    v-model="page.data.description">
+                                <label for="document" class="form-label">Anexar Amparo Legal</label>
+                                <input @change="handleFile" type="file" name="document" class="form-control">
                             </div>
                         </div>
 
@@ -253,6 +331,7 @@ onMounted(() => {
                         </div>
                     </form>
                 </div>
+
             </div>
         </section>
     </main>
