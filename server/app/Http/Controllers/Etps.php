21cdfdb9428bb3f67978b6acfb39e395593fb1aa
Renan <alvesrenanpro@gmail.com>
2024-06-24 10:06:09 -0300

    feat: alterations on Etps

--- server/app/Http/Controllers/Etps.php
+++ server/app/Http/Controllers/Etps.php
@@ -42,7 +42,7 @@ class Etps extends Controller
 
     public function selects(Request $request)
     {
-        $comission = $request->key && $request->key == 'organ' ? Utils::map_select(Data::list(Comission::class, [
+        $comissions = $request->key && $request->key == 'organ' ? Utils::map_select(Data::list(Comission::class, [
             [
                 'column' => $request->key,
                 'operator' => '=',
@@ -51,11 +51,20 @@ class Etps extends Controller
             ]
         ], ['name'])) : Utils::map_select(Data::list(Comission::class));
 
+        $dfds = $request->key && $request->key == 'organ' ? Utils::map_select(Data::list(Dfd::class, [
+            [
+                'column' => $request->key,
+                'operator' => '=',
+                'value' => $request->search,
+                'mode' => 'AND'
+            ]
+        ], ['protocol'])) : Utils::map_select(Data::list(Dfd::class));
+
         return Response()->json([
-            'dfds' => Utils::map_select(Data::list(Dfd::class, order: ['code'])),
             'organs' => Utils::map_select(Data::list(Organ::class, order: ['name'])),
+            'dfds' => $dfds,
             'status' => Etp::list_status(),
-            'comissions' => $comission,
+            'comissions' => $comissions,
         ], 200);
     }
 }
