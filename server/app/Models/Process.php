335e7a1bd3fd88ba283cc272c4e01f6a7e515a4b
Renan <alvesrenanpro@gmail.com>
2024-07-01 09:48:41 -0300

    feat: add constants to processes

--- server/app/Models/Process.php
+++ server/app/Models/Process.php
@@ -48,4 +48,45 @@ class Process extends Model
     {
         return $this->hasOne(User::class, 'id', 'author');
     }
+
+    public static function list_modalitys(): array
+    {
+        return [
+            ['id' => 0, 'title' => 'Pregão'],
+            ['id' => 1, 'title' => 'Tomada de Preço'],
+            ['id' => 2, 'title' => 'Convite'],
+            ['id' => 3, 'title' => 'Concorrência'],
+            ['id' => 4, 'title' => 'Registro de Preço'],
+            ['id' => 5, 'title' => 'Leilão'],
+            ['id' => 6, 'title' => 'Concurso'],
+            ['id' => 7, 'title' => 'Dispensa'],
+            ['id' => 8, 'title' => 'Inexigibiliadade'],
+            ['id' => 9, 'title' => 'Carona'],
+            ['id' => 10, 'title' => 'Chamada'],
+        ];
+    }
+
+    public static function list_situations(): array
+    {
+        return [
+            ['id' => 0, 'title' => 'Aberta'],
+            ['id' => 1, 'title' => 'Adiada'],
+            ['id' => 2, 'title' => 'Anulado'],
+            ['id' => 3, 'title' => 'Cancelado'],
+            ['id' => 4, 'title' => 'Deserta'],
+            ['id' => 5, 'title' => 'Finalizada'],
+            ['id' => 6, 'title' => 'Fracassada'],
+            ['id' => 7, 'title' => 'Revogada'],
+        ];
+    }
+
+    public static function list_types(): array
+    {
+        return [
+            ['id' => 0, 'title' => 'Menor Preço'],
+            ['id' => 1, 'title' => 'Melhor Técnica'],
+            ['id' => 2, 'title' => 'Preço e Técnica'],
+            ['id' => 3, 'title' => 'Outro'],
+        ];
+    }
 }
