81c85a51c004f630a20f6a274b87496c2f786f98
octuspi <octuspi@gmail.com>
2024-07-15 11:41:51 -0300

    fix border-radius td in tablelistreport view

--- client/src/views/reports/TableListReport.vue
+++ client/src/views/reports/TableListReport.vue
@@ -34,7 +34,7 @@ function getdata(data, obj, key, cast = null, subject = 'id') {
         <i class="bi bi-grip-vertical"></i> {{ (body.length).toString().padStart(2, '0') }} Registros Localizados
     </p>
     <div v-if="body.length" class="table-responsive-sm">
-        <table class="table-borderless table-striped table-hover" :class="props.smaller ? 'table tablesm' : 'table'">
+        <table :class="props.smaller ? 'table tablesm' : 'table'">
             <thead v-if="props.header">
                 <tr>
                     <th scope="col" v-for="h in props.header" :key="h.key">
@@ -73,6 +73,7 @@ th,
 td {
     border: 1px solid black;
     border-collapse: collapse;
+    border-radius: 0;
 }
 
 td {
