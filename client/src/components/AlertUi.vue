ff8bf07bc685a36db137232590f6669e75fd9522
octuspi <octuspi@gmail.com>
2024-08-12 19:07:01 -0300

    chore: add efect close to AlertUi

--- client/src/components/AlertUi.vue
+++ client/src/components/AlertUi.vue
@@ -3,6 +3,7 @@ import { ref, watch } from 'vue';
 
 const props = defineProps({ alert:{ type: Object, required: true } })
 const alert = ref(props.alert)
+const efect = ref(false)
 
 const alerts   = {
     success: { style: 'alert-success', icon: 'checkmark-circle-outline', title: 'Sucesso!', msg: 'Operação realizada com sucesso!', btn:'btn-alert-success' },
@@ -11,20 +12,28 @@ const alerts   = {
     info: { style: 'alert-info', icon: 'information-circle-outline', title: 'Informe!', msg: '', btn:'btn-alert-info' }
 }
 
+function close_alert() {
+    efect.value = true
+    setTimeout(() => {
+        alert.value.show = false;
+    }, 200);
+}
+
 watch(() => props.alert, (newValue) => {
+    efect.value = false
    alert.value = newValue
 });
 </script>
 
 <template>
     <div class="wall w-100 vh-100 position-fixed top-0 start-0" :class="alert.show ? 'd-block' : 'd-none'">
-        <div class="position-absolute top-50 start-50 translate-middle box-alert p-4 text-center shadow-sm">
+        <div class="position-absolute top-50 start-50 translate-middle box-alert p-4 text-center shadow-sm" :class="{'efect-down' : efect} ">
             <ion-icon :name="alerts[alert.data.type]?.icon" class="fs-1 p-0 m-0" :class="alerts[alert.data.type]?.style"></ion-icon>
             <h2 class="p-0 m-0 mb-3 mt-1" :class="alerts[alert.data.type]?.style">{{ alerts[alert.data.type]?.title }}</h2>
             <p class="p-0 m-0 small">{{ alerts[alert.data.type]?.msg }}</p>
             <p class="p-0 m-0 small">{{ alert?.data.msg }}</p>
 
-            <button @click="alert.show = false" type="button" class="btn btn-alert shadow-sm mx-auto mt-4" :class="alerts[alert.data.type]?.btn">
+            <button @click="close_alert" type="button" class="btn btn-alert shadow-sm mx-auto mt-4" :class="alerts[alert.data.type]?.btn">
                 <ion-icon name="chevron-down-outline" class="fs-2"></ion-icon>
             </button>
         </div>
@@ -37,6 +46,11 @@ watch(() => props.alert, (newValue) => {
         min-height: 120px;
         border-radius: 20px;
         background-color: var(--color-background-soft);
+    }
+
+    .efect-down{
+        opacity: 0;
+        transition: 0.5s;
     }
 
     .alert-success {
