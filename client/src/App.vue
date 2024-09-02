24b2c939a4299071b5e5ba5c19ce7c5517fa4c69
octuspi <octuspi@gmail.com>
2024-09-02 09:28:13 -0300

    feat: add btn system manual

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,6 +5,7 @@ import theme from '@/stores/theme'
 
 import AlertUi from './components/AlertUi.vue';
 import ModalDeleteUi from './components/ModalDeleteUi.vue';
+import ModalManualUi from './components/manual/ModalManualUi.vue';
 
 const alert = ref({ show: false, data: {} })
 const list = ref([])
@@ -18,8 +19,10 @@ onMounted(() => {
 <template>
 
     <div id="load-wall" class="load-wall d-none">
-    <img id="load-img" class="load-img" src="./assets/imgs/load.svg">
-  </div>
+        <img id="load-img" class="load-img" src="./assets/imgs/load.svg">
+    </div>
+
+    <ModalManualUi/>
 
     <AlertUi :alert="alert" />
 
