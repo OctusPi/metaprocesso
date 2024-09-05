67eaad1358b98315b6043e6202a2314bc736c24e
octuspi <octuspi@gmail.com>
2024-09-05 02:24:10 -0300

    fix: evn client end base mail get env

--- server/app/Mail/BaseMail.php
+++ server/app/Mail/BaseMail.php
@@ -12,9 +12,9 @@ class BaseMail extends Mailable
     protected string $renewRoute = 'renew';
 
     public function __construct(){
-        $this->system = env('APP_NAME','');
-        $this->sender = 'Administração ' . env('APP_NAME', '');
-        $this->appUrl = env('CLIENT_URL', '');
+        $this->system = getenv('APP_NAME');
+        $this->sender = 'Administração ' . getenv('APP_NAME');
+        $this->appUrl = getenv('CLIENT_URL');
     }
 
     protected function makeUrl(array $routes): string
