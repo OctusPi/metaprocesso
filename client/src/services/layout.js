e0075074631dd0e4064911ba9ec0bb19c4edf475
Renan <alvesrenanpro@gmail.com>
2024-09-04 22:59:11 -0300

    fix: change he order of merge in utils merge tablelistselect

--- client/src/services/layout.js
+++ client/src/services/layout.js
@@ -1,29 +1,37 @@
 import { reactive } from "vue"
 import Data from "./data"
 import storeOrgan from '@/stores/organ';
+import PseudoData from "./pseudodata";
+
+function buildPage(preloaded = {}) {
+    const organ = storeOrgan.get_organ()
+    return reactive({
+        url: "",
+        organ: organ,
+        organ_name: organ?.name,
+        ui: { list: true },
+        datalist: [],
+        header: [],
+        selects: {},
+        data: {},
+        search: {},
+        rules: {},
+        valids: {},
+        ...preloaded
+    })
+}
 
 export default {
     new: (emit, preloaded = {}) => {
-        
-        const organ = storeOrgan.get_organ()
-
-        const page = reactive({
-            url: "",
-            organ: organ,
-            organ_name: organ?.name,
-            ui: { list: true },
-            datalist: [],
-            header: [],
-            selects: {},
-            data: {},
-            search: {},
-            rules: {},
-            valids: {},
-            ...preloaded
-        })
-
+        const page = buildPage(preloaded)
         const data = new Data(page, emit)
 
         return [page, data]
     },
+    pseudo: (emit, preloaded = {}) => {
+        const page = buildPage(preloaded)
+        const data = new PseudoData(page, emit)
+
+        return [page, data]
+    }
 }
