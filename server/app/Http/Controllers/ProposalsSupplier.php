30d330290b3828a4b58e909c1f0101313a142ad2
octuspi <octuspi@gmail.com>
2024-09-24 12:09:49 -0300

    fix: add update date_fin, hour_fin and update status price_record

--- server/app/Http/Controllers/ProposalsSupplier.php
+++ server/app/Http/Controllers/ProposalsSupplier.php
@@ -9,58 +9,74 @@ use App\Models\DfdItem;
 use App\Models\Proposal;
 use App\Models\PriceRecord;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class ProposalsSupplier extends Controller
 {
+    /**
+     * ProposalsSupplier constructor.
+     */
     public function __construct()
     {
         parent::__construct(Proposal::class, null, false);
     }
 
-    public function check(Request $request)
+    /**
+     * Verifica o status de uma proposta com base no token fornecido.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function check(Request $request): JsonResponse
     {
-        $proposal = Proposal::with(['organ', 'process', 'supplier', 'price_record'])->firstWhere('token', $request->token);
+        $proposal = Proposal::with(['organ', 'process', 'supplier', 'price_record'])
+            ->firstWhere('token', $request->token);
 
-        if(is_null($proposal)){
+        if (is_null($proposal)) {
             return response()->json(Notify::warning('Solicitação de coleta de preço não localizada....'), 404);
         }
 
-        if($proposal->status == Proposal::S_FINISHED){
+        if ($proposal->status == Proposal::S_FINISHED) {
             return response()->json(Notify::success('Solicitação de coleta de preço já enviada...'), 403);
         }
 
         $price_record = PriceRecord::with('organ')->find($proposal->price_record);
-        if(is_null($price_record) || now()->greaterThan(Dates::ptbrToUtc($price_record->date_fin))){
+        if (is_null($price_record) || now()->greaterThan(Dates::ptbrToUtc($price_record->date_fin))) {
             return response()->json(Notify::warning('Solicitção de registro de coleta de preço expirada...'), 403);
         }
 
-        if($proposal->status < Proposal::S_OPENED){
+        if ($proposal->status < Proposal::S_OPENED) {
             $proposal->status = Proposal::S_OPENED;
             $proposal->save();
         }
 
+        $proposalArray = $proposal->toArray();
+        $proposalArray['dfd_items'] = $this->dfdItems($proposalArray['process']['dfds']);
 
-        $proposal = $proposal->toArray();
-        $proposal['dfd_items'] = $this->dfdItems($proposal['process']['dfds']);
-
-        return response()->json($proposal);
+        return response()->json($proposalArray);
     }
 
-    public function save(Request $request)
+    /**
+     * Salva os dados de uma proposta com as validações necessárias.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function save(Request $request): JsonResponse
     {
         $instance = $this->model::find($request->id);
 
-        if(is_null($instance)){
+        if (is_null($instance)) {
             return response()->json(Notify::warning('Solicitação de coleta não localizada...'), 404);
         }
 
-        if($request->status == Proposal::S_FINISHED){
-            if(is_null($request->document)){
-                return response()->json(Notify::warning('É necessário anexar a proposta assinada para confirmar o envio...'),400);
+        if ($request->status == Proposal::S_FINISHED) {
+            if (is_null($request->document)) {
+                return response()->json(Notify::warning('É necessário anexar a proposta assinada para confirmar o envio...'), 400);
             }
 
-            if(is_null($request->code) || $request->code != $instance->protocol){
-                return response()->json(Notify::warning('Código de validação incorreto...'),403);
+            if (is_null($request->code) || $request->code != $instance->protocol) {
+                return response()->json(Notify::warning('Código de validação incorreto...'), 403);
             }
         }
 
@@ -70,14 +86,26 @@ class ProposalsSupplier extends Controller
             $upload->remove($instance->document);
         }
 
-        if ($instance->update($upload->mergeUploads($request->all()))) {
-            return response()->json(Notify::success('Proposta salva com sucesso'),200);
+        $dataToUpdate = array_merge($request->all(), [
+            'date_fim' => date('d/m/Y'),
+            'hour_fin' => date('H:i:s')
+        ]);
+
+        if ($instance->update($upload->mergeUploads($dataToUpdate))) {
+            $this->updateStatusPriceRecord($instance->price_record);
+            return response()->json(Notify::success('Proposta salva com sucesso'), 200);
         } else {
-            return response()->json(Notify::error('Falha ao gravar proposta'),500);
+            return response()->json(Notify::error('Falha ao gravar proposta'), 500);
         }
     }
 
-    private function dfdItems(?array $dfds):array
+    /**
+     * Retorna uma lista de itens do DFD somados por quantidade.
+     *
+     * @param array|null $dfds
+     * @return array
+     */
+    private function dfdItems(?array $dfds): array
     {
         $items = array_map(function ($id) {
             return DfdItem::where('dfd', '=', $id)->with('item')->get()->toArray();
@@ -85,7 +113,7 @@ class ProposalsSupplier extends Controller
 
         $result = array_reduce($items, function ($carry, $subArray) {
             foreach ($subArray as $item) {
-                $key = $item['item']['id'] ;
+                $key = $item['item']['id'];
                 if (isset($carry[$key])) {
                     $carry[$key]['quantity'] += $item['quantity'];
                 } else {
@@ -96,6 +124,34 @@ class ProposalsSupplier extends Controller
         }, []);
 
         return array_values($result);
+    }
+
+    /**
+     * Atualiza o status do registro de preço com base nas propostas associadas.
+     * O status será atualizado para S_FINISHED apenas se todas as propostas tiverem esse status.
+     *
+     * @param int $id
+     * @return void
+     */
+    private function updateStatusPriceRecord(int $id): void
+    {
+        // Obtém todas as propostas relacionadas ao registro de preço
+        $proposals = Proposal::where('price_record', $id)->get();
+
+        if (!$proposals->isEmpty()) {
+            // Verifica se todas as propostas têm o status S_FINISHED
+            $allFinished = $proposals->every(function ($proposal) {
+                return $proposal->status == Proposal::S_FINISHED;
+            });
+
+            // Atualiza o status do PriceRecord com base nas propostas
+            $price_record = PriceRecord::find($id);
+            if ($price_record) {
+                $price_record->update([
+                    'status' => $allFinished ? PriceRecord::S_FINISHED : PriceRecord::S_PENDING
+                ]);
+            }
+        }
     }
 
 }
