3db867beab1a60874c5bb94c431aac62921ed0d8
Renan <alvesrenanpro@gmail.com>
2024-10-18 12:36:30 -0300

    feat: add view to pca

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -163,6 +163,12 @@ const router = createRouter({
       component: () => import('../views/RefTermsView.vue')
     },
     {
+      path: '/pca',
+      name: 'pca',
+      meta: { auth: true },
+      component: () => import('../views/PCAView.vue')
+    },
+    {
       path: '/forbidden',
       name: 'forbidden',
       component: () => import('../views/ForbiddenView.vue')
