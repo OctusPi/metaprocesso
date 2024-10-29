cab1de573f690ff0d2911e8cdff02e8d0f232f72
Renan <alvesrenanpro@gmail.com>
2024-10-28 22:13:28 -0300

    feat: add protocol to PCA View

--- client/src/views/PCAView.vue
+++ client/src/views/PCAView.vue
@@ -1,144 +1,150 @@
 <script setup>
-  import TableList from '@/components/table/TableList.vue';
-  import NavMainUi from '@/components/NavMainUi.vue';
-  import HeaderMainUi from '@/components/HeaderMainUi.vue';
-  import FooterMainUi from '@/components/FooterMainUi.vue';
-  import Layout from '@/services/layout';
-  import Actions from '@/services/actions';
-  import Mounts from '@/services/mounts';
-  import masks from '@/utils/masks';
-  import { onMounted, ref, watch } from 'vue';
-  import DfdDetails from '@/components/DfdDetails.vue';
-  import http from '@/services/http';
+import TableList from '@/components/table/TableList.vue';
+import NavMainUi from '@/components/NavMainUi.vue';
+import HeaderMainUi from '@/components/HeaderMainUi.vue';
+import FooterMainUi from '@/components/FooterMainUi.vue';
+import Layout from '@/services/layout';
+import Actions from '@/services/actions';
+import Mounts from '@/services/mounts';
+import masks from '@/utils/masks';
+import { onMounted, ref, watch } from 'vue';
+import DfdDetails from '@/components/DfdDetails.vue';
+import http from '@/services/http';
 import { Mask } from 'maska';
+import utils from '@/utils/utils';
+const emit = defineEmits(['callAlert', 'callUpdate'])
 
-  const emit = defineEmits(['callAlert', 'callUpdate'])
+const props = defineProps({
+  datalist: { type: Array, default: () => [] }
+})
 
-  const props = defineProps({
-    datalist: { type: Array, default: () => [] }
-  })
-
-  const [page, pageData] = Layout.new(emit, {
-    url: '/pca',
-    datalist: props.datalist,
-    header: [
-      { title: 'ANO E EMISSÃO', key: 'reference_year', sub: [{ key: 'emission' }] },
-      { key: 'comission.name', title: 'COMISSÃO' },
-      { key: 'observations', title: 'OBSERVAÇÕES' },
-      { key: 'status', title: 'STATUS' },
+const [page, pageData] = Layout.new(emit, {
+  url: '/pca',
+  datalist: props.datalist,
+  header: [
+    { title: 'ANO E EMISSÃO', key: 'reference_year', sub: [{ key: 'emission' }] },
+    { key: 'comission.name', title: 'COMISSÃO' },
+    { key: 'observations', title: 'OBSERVAÇÕES' },
+    { key: 'status', title: 'STATUS' },
+  ],
+  rules: {
+    comission_id: 'required',
+    reference_year: 'required',
+    emission: 'required',
+    price: 'required',
+    status: 'required',
+  },
+  dfd: {
+    year: '',
+    datalist: [],
+    pca: {},
+    headers: [
+      { key: 'date_ini', title: 'IDENTIFICAÇÃO', sub: [{ key: 'protocol' }] },
+      { key: 'demandant.name', title: 'DEMANDANTE' },
+      { key: 'ordinator.name', title: 'ORDENADOR' },
+      { key: 'unit.name', title: 'ORIGEM' },
+      { title: 'OBJETO', sub: [{ key: 'description' }] },
+      { key: 'status', title: 'SITUAÇÃO' },
     ],
-    rules: {
-      comission_id: 'required',
-      reference_year: 'required',
-      emission: 'required',
-      price: 'required',
-      status: 'required',
-    },
-    dfd: {
-      year: '',
-      datalist: [],
-      pca: {},
-      headers: [
-        { key: 'date_ini', title: 'IDENTIFICAÇÃO', sub: [{ key: 'protocol' }] },
-        { key: 'demandant.name', title: 'DEMANDANTE' },
-        { key: 'ordinator.name', title: 'ORDENADOR' },
-        { key: 'unit.name', title: 'ORIGEM' },
-        { title: 'OBJETO', sub: [{ key: 'description' }] },
-        { key: 'status', title: 'SITUAÇÃO' },
-      ],
-    },
-  })
+  },
+})
 
-  const dfdsChart = ref({})
+const dfdsChart = ref({})
 
-  function populateDfds(dataset) {
-    const keys = Object.keys(dataset)
-    const values = Object.values(dataset)
-    dfdsChart.value = {
-      total: values.length,
-      series: [{
-        name: 'Quantidade',
-        data: values
-      }],
-      chartOptions: {
-        chart: {
-          id: 'dfdsChart',
-          offsetX: -4,
-          width: "100%",
-          fontFamily: 'Inter, Helvetica, Arial',
-          toolbar: {
-            show: false
-          }
+function populateDfds(dataset) {
+  const keys = Object.keys(dataset)
+  const values = Object.values(dataset)
+  dfdsChart.value = {
+    total: values.length,
+    series: [{
+      name: 'Quantidade',
+      data: values
+    }],
+    chartOptions: {
+      chart: {
+        id: 'dfdsChart',
+        offsetX: -4,
+        width: "100%",
+        fontFamily: 'Inter, Helvetica, Arial',
+        toolbar: {
+          show: false
+        }
+      },
+      colors: ['var(--color-base)'],
+      grid: {
+        borderColor: 'var(--color-input-focus)',
+        position: 'front',
+        strokeDashArray: 7,
+        sort: true
+      },
+      plotOptions: {
+        bar: {
+          borderRadius: 8,
+          barHeight: "80%",
+          borderRadiusApplication: 'end',
+          horizontal: true,
+        }
+      },
+      dataLabels: {
+        enabled: false,
+        textAnchor: 'start',
+        formatter: function (_, opt) {
+          return opt.w.globals.labels[opt.dataPointIndex]
         },
-        colors: ['var(--color-base)'],
-        grid: {
-          borderColor: 'var(--color-input-focus)',
-          position: 'front',
-          strokeDashArray: 7,
-          sort: true
+      },
+      yaxis: {
+        labels: {
+          color: 'var(--color-input-focus)',
+          show: true
         },
-        plotOptions: {
-          bar: {
-            borderRadius: 8,
-            barHeight: "80%",
-            borderRadiusApplication: 'end',
-            horizontal: true,
-          }
-        },
-        dataLabels: {
-          enabled: false,
-          textAnchor: 'start',
-          formatter: function (_, opt) {
-            return opt.w.globals.labels[opt.dataPointIndex]
-          },
-        },
-        yaxis: {
-          labels: {
-            color: 'var(--color-input-focus)',
-            show: true
-          },
-        },
-        xaxis: {
-          categories: keys,
-          stepSize: 1,
-          axisBorder: {
-            show: true,
-            color: 'var(--color-input-focus)',
-          }
+      },
+      xaxis: {
+        categories: keys,
+        stepSize: 1,
+        axisBorder: {
+          show: true,
+          color: 'var(--color-input-focus)',
         }
       }
     }
   }
+}
 
-  const listDfdsForPCA = (id) => {
-    const pca = page.datalist.find(o => o.id === id)
-    http.post(`${page.url}/list_dfds/${pca?.reference_year}`, {}, emit, (res) => {
-      page.dfd.pca = pca
-      page.dfd.year = pca?.reference_year
-      page.dfd.datalist = res.data?.datalist
-      page.dfd.estimated = res.data?.estimated
-      populateDfds(res.data?.dfds_chart ?? [])
-      pageData.ui('prepare')
+const listDfdsForPCA = (id) => {
+  const pca = page.datalist.find(o => o.id === id)
+  http.post(`${page.url}/list_dfds/${pca?.reference_year}`, {}, emit, (res) => {
+    page.dfd.pca = pca
+    page.dfd.year = pca?.reference_year
+    page.dfd.datalist = res.data?.datalist
+    page.dfd.estimated = res.data?.estimated
+    populateDfds(res.data?.dfds_chart ?? [])
+    pageData.ui('prepare')
+  })
+}
+
+const dfd_details = (id) => {
+  if (page.dfd.datalist) {
+    page.dfd.data = page.dfd.datalist.find(obj => obj.id === id)
+    http.get(`${page.url}/list_dfd_items/${id}`, emit, (resp) => {
+      page.dfd.data.items = resp.data
     })
   }
+}
 
-  const dfd_details = (id) => {
-    if (page.dfd.datalist) {
-      page.dfd.data = page.dfd.datalist.find(obj => obj.id === id)
-      http.get(`${page.url}/list_dfd_items/${id}`, emit, (resp) => {
-        page.dfd.data.items = resp.data
-      })
+watch(() => page.ui.register, (newdata) => {
+    if (newdata && page.data.id == null) {
+        page.data.protocol = utils.dateProtocol(page.organ?.id)
     }
-  }
+})
 
-  watch(() => props.datalist, (newdata) => {
-    page.datalist = newdata
-  })
+watch(() => props.datalist, (newdata) => {
+  page.datalist = newdata
+})
 
-  onMounted(() => {
-    pageData.selects()
-    pageData.list()
-  })
+onMounted(() => {
+  pageData.selects()
+  pageData.list()
+})
 
 </script>
 
@@ -245,12 +251,12 @@ import { Mask } from 'maska';
                   menu-class-name="dp-custom-menu" />
               </div>
               <div class="col-sm-12 col-md-4">
-                <label for="price" class="form-label">Preço</label>
-                <input type="text" name="price" class="form-control"
-                  :class="{ 'form-control-alert': page.valids.price }" id="price"
-                  placeholder="R$ 0.00" v-model="page.data.price" v-maska:[masks.maskmoney] />
+                <label for="protocol" class="form-label">Protocolo</label>
+                <input type="text" name="protocol" class="form-control"
+                  :class="{ 'form-control-alert': page.valids.protocol }" id="protocol" v-model="page.data.protocol"
+                  placeholder="Protocolo">
               </div>
-              <div class="col-sm-12 col-md-8">
+              <div class="col-sm-12 col-md-4">
                 <label for="comission" class="form-label">Comissão/Equipe de
                   Planejamento</label>
                 <select name="comission" class="form-control"
@@ -265,6 +271,12 @@ import { Mask } from 'maska';
                   Ao selecionar a comissão/equipe de planejamento seus
                   integrantes serão vinculados ao documento
                 </div>
+              </div>
+              <div class="col-sm-12 col-md-4">
+                <label for="price" class="form-label">Preço</label>
+                <input type="text" name="price" class="form-control"
+                  :class="{ 'form-control-alert': page.valids.price }" id="price" placeholder="R$ 0.00"
+                  v-model="page.data.price" v-maska:[masks.maskmoney] />
               </div>
               <div class="col-sm-12 col-md-4">
                 <label for="status" class="form-label">Status</label>
@@ -320,7 +332,7 @@ import { Mask } from 'maska';
                 <div class="p-4 h-100 mb-5">
                   <div class="chart-heading">
                     <span>
-                      <ion-icon name="information-circle-outline" />
+                      <ion-icon name="information-circle" />
                     </span>
                     <div>
                       <h1>Informações Gerais</h1>
@@ -392,7 +404,7 @@ import { Mask } from 'maska';
 </template>
 
 <style scoped>
-  .graph {
-    min-height: 300px;
-  }
+.graph {
+  min-height: 300px;
+}
 </style>
